9ed53d8cb7bb11f14dd85c2826b7e765d7068c6f
cmartingu <152396741+cmartingu@users.noreply.github.com>
2024-01-29 10:19:13 +0000

    historial y exit

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "Libft/libft.h"
+#include <signal.h>
 
 int main(void)
 {
@@ -12,8 +14,13 @@ int main(void)
 		comando = readline("minishell>");
 		if (comando && *comando)
         	add_history(comando);
-		else
-			exit(1);
+		if (ft_strncmp(comando, "history -c", 10) == 0)
+			rl_clear_history();
+		else if (ft_strncmp(comando, "exit", 4) == 0)
+		{
+			printf("exit\n");
+			exit(0);
+		}
 	}
 	return (0);
 }
