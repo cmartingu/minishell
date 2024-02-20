cefe4907d69095af227337d4d17be6d5caccd2d9
cmartingu <152396741+cmartingu@users.noreply.github.com>
2024-02-20 13:29:55 +0100

    Update ctrl_handlers.c

--- ctrl_handlers.c
+++ ctrl_handlers.c
@@ -1,14 +1,28 @@
-#include "minishell.c"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ctrl_handlers.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlos-m <carlos-m@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 12:19:30 by carlos-m          #+#    #+#             */
+/*   Updated: 2024/02/20 12:19:31 by carlos-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	ctrl_C_handler(int sig)
+#include "minishell.h"
+
+void	ctrl_c_handler(int sig)
 {
+	sig = 0;
 	printf("\n");
-	rl_on_new_line(); // Prepara readline para recibir una nueva línea
-	rl_replace_line("", 0); // Limpia el buffer de la línea actual en readline
-	rl_redisplay(); // Vuelve a mostrar el prompt
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	ctrl_bar_handler(int sig)
 {
-	return;
+	sig = 0;
+	return ;
 }
