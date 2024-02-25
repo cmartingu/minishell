0896402468c77ac3fe7aa1029a46c06c6220915f
cmartingu <152396741+cmartingu@users.noreply.github.com>
2024-02-25 15:22:46 +0100

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -12,179 +12,113 @@
 
 #include "minishell.h"
 
-int	pipes_quant(char **tokens)
-{
-	int		pipes;
-	int		i;
-
-	i = 0;
-	pipes = 0;
-	while (tokens[i] != NULL)
-	{
-		if (*(tokens[i]) == '|')
-			pipes++;
-		i++;
-	}
-	return (pipes);
-}
-
-void	add_infile(t_process *proceso, char *inf)
-{
-	t_fileobject	*new_file;
-	t_fileobject	*aux_file;
-
-	new_file = malloc(sizeof(new_file));
-	if (!new_file)
-	{
-		printf("Error, malloc error\n");
-		exit(1);
-	}
-	new_file->filename = ft_strdup(inf);
-	new_file->next = NULL;
-	if (!(proceso->infile))
-		proceso->infile = new_file;
-	else
-	{
-		aux_file = proceso->infile;
-		while (aux_file->next != NULL)
-			aux_file = aux_file->next;
-		aux_file->next = new_file;
-	}
-}
-
-void	add_outfile(t_process *proceso, char *outf)
-{
-	t_fileobject	*new_file;
-	t_fileobject	*aux_file;
-
-	new_file = malloc(sizeof(new_file));
-	if (!new_file)
-	{
-		printf("Error, malloc error\n");
-		exit(1);
-	}
-	new_file->filename = ft_strdup(outf);
-	new_file->next = NULL;
-	if (!(proceso->outfile))
-		proceso->outfile = new_file;
-	else
-	{
-		aux_file = proceso->outfile;
-		while (aux_file->next != NULL)
-			aux_file = aux_file->next;
-		aux_file->next = new_file;
-	}
-}
-
-void	free_arr(char **arr)
+int	contador_comillas(char  *comando)
 {
 	int	i;
+	int	cont;
 
+	if (!comando)
+		return(-1);
 	i = 0;
-	while (arr[i])
+	cont = 0;
+	while (comando[i] != '\0')
 	{
-		free(arr[i]);
+		if (comando[i] == '"')
+			cont++;
 		i++;
 	}
-	free(arr);
+	if (cont == 0 || cont % 2 == 0)
+		return(0);
+	return(-1);
 }
 
-char **ft_realloc_doble(char **cmd, char *str, int tam)
+void	process_creation(t_process *proceso, char **tokens, int *i, int *cmd_quant)
 {
-	char	**final_cmd;
-
-	final_cmd = malloc((tam + 1) * sizeof(char *));
-	if (!final_cmd)
+	if (ft_strncmp(tokens[*i], "<", ft_strlen(tokens[*i])) == 0)
 	{
-		printf("Error, malloc error");
-		exit(1);
+		(*i)++;
+		if (*(tokens[*i]) != '<' && *(tokens[*i]) != '>' && *(tokens[*i]) != '|')
+			add_infile(proceso, tokens[*i]);
 	}
-	final_cmd[tam] = NULL;
-	final_cmd[tam - 1] = ft_strdup(str);
-	tam--;
-	while (tam--)
-		final_cmd[tam] = ft_strdup(cmd[tam]);
-	free_arr(cmd);
-	return (final_cmd);
-}
-
-void	add_cmd(t_process *proceso, char *cmd)
-{
-	if (!(proceso->command))
+	else if (ft_strncmp(tokens[*i], ">", ft_strlen(tokens[*i])) == 0)
 	{
-		proceso->command = malloc(2 * sizeof(char *));
-		if (!(proceso->command))
-		{
-			printf("Error, malloc error");
-			exit(1);
-		}
-		(proceso->command)[0] = ft_strdup(cmd);
-		(proceso->command)[1] = NULL;
+		(*i)++;
+		if (*(tokens[*i]) != '<' && *(tokens[*i]) != '>' && *(tokens[*i]) != '|')
+			add_outfile(proceso, tokens[*i]);
 	}
-	else//Ya hay comando, hay que guardarlo a continuacion
+	else
 	{
+		(*cmd_quant)++;
+		add_cmd(proceso, tokens[*i], *cmd_quant);
 	}
+	(*i)++;
 }
 
 t_process	*procesos(int nb, char **tokens)
 {
 	t_process	*proceso;
+	t_process	*proceso_aux;
 	int			i;
 	int			cmd_quant;
 
 	i = -1;
+	proceso_aux = malloc(sizeof(t_process));
+	proceso_aux->next = NULL;
 	while (nb--)
 	{
 		i++;
 		cmd_quant = 0;
 		proceso = malloc(sizeof(t_process));
+		proceso->next = NULL;
+		add_process_back(proceso_aux, proceso);
 		while (tokens[i] != NULL && *(tokens[i]) != '|')
-		{
-			if (ft_strncmp(tokens[i], "<", ft_strlen(tokens[i])) == 0)
-			{
-				i++;
-				if (*(tokens[i]) != '<' && *(tokens[i]) != '>' && *(tokens[i]) != '|')
-					add_infile(proceso, tokens[i]);
-			}
-			if (ft_strncmp(tokens[i], ">", ft_strlen(tokens[i])) == 0)
-			{
-				i++;
-				if (*(tokens[i]) != '<' && *(tokens[i]) != '>' && *(tokens[i]) != '|')
-					add_outfile(proceso, tokens[i]);
-			}
-			else
-			{
-				cmd_quant++;
-				add_cmd(proceso, tokens[i]);
-			}
-			i++;
-		}
+			process_creation(proceso, tokens, &i, &cmd_quant);
 	}
-	/*printf("Infiles\n\n");
-	while (proceso->infile)
-	{
-		printf("%s\n", proceso->infile->filename);
-		proceso->infile = proceso->infile->next;
-	}
-	printf("Outfiles:\n\n");
-	while (proceso->outfile)
-	{
-		printf("%s\n", proceso->outfile->filename);
-		proceso->outfile = proceso->outfile->next;
-	}*/
-	return (NULL);
+	proceso = proceso_aux->next;
+	free(proceso_aux);
+	return (proceso);
 }
 
 void	tokenization_string(char *cmd)
 {
-	char    **tokens;
-	int		tam;
-	int		forks;
+	char    	**tokens;
+	t_process	*proceso;
+	int			tam;
+	int			forks;
 
+	if (contador_comillas(cmd) == -1)
+	{
+		printf("Minishell error: Acuerdate de cerrar las comillas\n");
+		exit(1);
+	}
 	tam = token_quant(cmd);
 	tokens = save_tokens(tam, &cmd);
 	forks = pipes_quant(tokens) + 1;
-	procesos(forks, tokens);
+	proceso = procesos(forks, tokens);
+	/*while (proceso != NULL)
+	{
+		i = 0;
+		printf("\n\n");
+		printf("Infiles:\n");
+		while (proceso->infile)
+		{
+			printf("%s\n", proceso->infile->filename);
+			proceso->infile = proceso->infile->next;
+		}
+		printf("Outfiles:\n");
+		while (proceso->outfile)
+		{
+			printf("%s\n", proceso->outfile->filename);
+			proceso->outfile = proceso->outfile->next;
+		}
+		printf("Comando:\n");
+		while ((proceso->command)[i] != '\0')
+		{
+			printf("%s\n", (proceso->command)[i]);
+			i++;
+		}
+		proceso = proceso->next;
+	}*/
 }
 
 int main(void)
